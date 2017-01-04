e0d3a530b12b3b206675b702359c793c893aaebc
Pim Snel <pim@lingewoud.nl>
2017-01-05 00:27:33 +0100

    reshuffle

--- test/acmesmith/http/path_test.rb
+++ test/acmesmith/http/path_test.rb
@@ -4,8 +4,4 @@ class Acmesmith::Http::PathTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Acmesmith::Http::Path::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'acmesmith/http/path'
+require 'acmesmith/http-path'
 
 require 'minitest/autorun'
